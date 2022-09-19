463d2e53f8da822139c5db407f4c3c1d244ba2a2
rahulramnathbhor <112869809+rahulramnathbhor@users.noreply.github.com>
2022-09-19 16:53:41 +0530

    Add project files.

---
+++ RefKeyword/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefKeyword
+{
+    internal class Program
+    {
+        static void Swap(ref int a, ref int b)
+        {
+            int temp;
+            temp = a;
+            a = b;
+            b = temp;
+        }
+        static void Main(string[] args)
+        {
+            int a = 150, b = 320;
+            Swap(ref a, ref b); // call by ref / address
+            Console.WriteLine($"a = {a}  b= {b}");
+        }
+
+
+
+    }
+}
